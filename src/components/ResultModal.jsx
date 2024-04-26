0f0ef175870c4a2aba6abf686bbd0b89796f09c0
Rafael <rafaelvoss94@gmail.com>
2024-04-26 19:39:25 -0300

    minor change on forwardRef

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,7 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
 import { createPortal } from 'react-dom';
 
-const ResultModal = forwardRef(function ResultModal({ targetTime, timeRemaining, onReset }, ref) {
+const ResultModal = forwardRef(({ targetTime, timeRemaining, onReset }, ref) => {
     const dialog = useRef();
     const userLost = timeRemaining <= 0;
     const formattedRemainingTime = (timeRemaining / 1000).toFixed(2);
